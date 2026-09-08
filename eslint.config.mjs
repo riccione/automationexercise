91a28cf6de84861d9fec260f3a43fbb88ca13dd1
riccione <28346232+riccione@users.noreply.github.com>
2026-09-08 15:49:59 +0200

    chore: clean Playwright template for assignment

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
-import js from "@eslint/js";
-import tseslint from "typescript-eslint";
-import playwright from "eslint-plugin-playwright";
-import eslintConfigPrettier from "eslint-config-prettier";
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import playwright from 'eslint-plugin-playwright';
+import eslintConfigPrettier from 'eslint-config-prettier';
 
 export default tseslint.config(
   {
--- playwright.config.ts
+++ playwright.config.ts
@@ -32,7 +32,7 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
-    baseURL: process.env.BASE_URL || 'https://playwright.dev/',
+    baseURL: process.env.BASE_URL || 'https://automationexercise.com',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
